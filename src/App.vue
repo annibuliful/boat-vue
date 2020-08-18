4e74525a95af03732d3c2c5b3ab8ce15118ea8a5
jarupong pajakgo <lagmanmc@gmail.com>
2020-08-18 22:23:56 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- {{ message }} -->
+    <input v-model="message" />
+    <p v-if="isShowMessage && message === 'a'">{{ message }}</p>
+    <!-- v-bind:key="index" -->
+    <div v-for="(value, index) in listTodos" :key="index + '-' + value">
+      {{ value }}, {{ index + "-" + value }}
+    </div>
+    <br />
+    <br />
+
+    <!-- Event Handler -->
+    {{ counter }}
+    <!-- v-on:click="counter++" -->
+    <button @click="onIncrementCounter">click</button>
+    <input @change="onChangeInputMessage" />
+    {{ inputMessage }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// 1,2,3,4
+// 0,1,2,3
 
+// 1,3,4
+// 0,1,2
+
+// 1,3,4 => temp
+// temp => old
+// old => rerender
+
+// 1  ,2  ,3  ,4
+// 0-1,1-2,2-3,3-4
+
+// 1  ,3  ,4, 5
+// 0-1,2-3,3-4,4-5
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      counter: 0,
+      message: "Hello",
+      isShowMessage: true,
+      listTodos: [1, 2, 3, 4],
+      inputMessage: ""
+    };
+  },
+  methods: {
+    onIncrementCounter: function() {
+      this.counter++;
+    },
+    onChangeInputMessage: function(event) {
+      this.inputMessage = event.target.value;
+    }
   }
 };
 </script>
